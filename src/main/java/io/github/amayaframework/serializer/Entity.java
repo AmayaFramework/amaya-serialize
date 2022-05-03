dde8abfc9ad71bad3fb185b8230bebc4c1fa9e70
RomanQed <gbakaldin@gmail.com>
2022-05-03 14:54:24 +0300

    Add @Documented to Entity Optimize SerializeConfigurator

--- src/main/java/io/github/amayaframework/serializer/Entity.java
+++ src/main/java/io/github/amayaframework/serializer/Entity.java
@@ -1,15 +1,13 @@
 package io.github.amayaframework.serializer;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * An annotation indicating that the controller or its method expects an instance of the specified class as the body.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
+@Documented
 public @interface Entity {
     Class<?> value();
 }
--- src/main/java/io/github/amayaframework/serializer/SerializeConfigurator.java
+++ src/main/java/io/github/amayaframework/serializer/SerializeConfigurator.java
@@ -6,24 +6,29 @@ import io.github.amayaframework.core.pipeline.NamedPipeline;
 import io.github.amayaframework.core.routes.MethodRoute;
 
 import java.util.Collection;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * The configurator that needs to be added to the builder if you need to use the functionality of the plugin.
  * <p>Added using {@link io.github.amayaframework.core.AmayaBuilder#addConfigurator(Configurator)}</p>
  */
 public final class SerializeConfigurator implements Configurator {
+    private final AtomicBoolean flag = new AtomicBoolean(false);
 
     @Override
     public void configureController(Controller controller) {
+        flag.set(false);
         Entity entity = controller.getControllerClass().getAnnotation(Entity.class);
         Collection<MethodRoute> routes = controller.getRoutes();
         if (entity != null) {
+            flag.set(true);
             routes.forEach(route -> route.set(Util.METHOD_ENTITY, entity.value()));
             return;
         }
         for (MethodRoute route : routes) {
             Entity routeEntity = route.getMethod().getAnnotation(Entity.class);
             if (routeEntity != null) {
+                flag.set(true);
                 route.set(Util.METHOD_ENTITY, routeEntity.value());
             }
         }
@@ -31,11 +36,15 @@ public final class SerializeConfigurator implements Configurator {
 
     @Override
     public void configureInput(NamedPipeline input) {
-        input.put(new DeserializeAction());
+        if (flag.get()) {
+            input.put(new DeserializeAction());
+        }
     }
 
     @Override
     public void configureOutput(NamedPipeline output) {
-        output.put(new SerializeAction());
+        if (flag.get()) {
+            output.put(new SerializeAction());
+        }
     }
 }
